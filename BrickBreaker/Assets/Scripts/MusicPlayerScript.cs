5b8a228759afa577fc756e83945b58d2ac0a0204
Karl Spiteri <a100134@mcast.org.mt>
2017-11-08 10:55:41 +0100

    Added my unity games 9

---
+++ BrickBreaker/Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScript : MonoBehaviour {
+
+    static MusicPlayerScript instance = null;
+	// Use this for initialization
+	private void Awake () {
+
+        if (instance == null)
+        {
+            instance = this;// this current object (my Music Player)
+            DontDestroyOnLoad(gameObject);
+        }
+        else// if not null, then a  Music Player is already running
+        {
+            Destroy(gameObject);
+            print("Duplicate Music Player is self destructing!");
+        }
+	}
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+    }
+    void Update () {
+		
+	}
+}
