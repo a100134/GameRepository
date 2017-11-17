eb7143ff485dce0e948bdbc55f9c15fb1581d9dd
Karl Spiteri <a100134@mcast.org.mt>
2017-11-17 14:20:39 +0100

    - Created a new scene - Set all objects as prefabs - Created a new level of brick - Set the game to automatically load new level - Added sound effects to ball

---
+++ BrickGame/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class LevelManager : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+   public void LoadNewScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void LoadNextScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        currentScene++;
+        SceneManager.LoadScene(currentScene);
+    }
+}
